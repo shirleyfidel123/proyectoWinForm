ed01c7585767046f5ecfbf551680a77a72272637
shirleyfidelet12d1Qgmail.com <shirleyfidelet12d1@gmail.com>
2022-11-01 22:50:49 -0300

    agregado de titulo

--- A/Form1.Designer.cs
+++ A/Form1.Designer.cs
@@ -29,24 +29,14 @@ namespace A
         /// </summary>
         private void InitializeComponent()
         {
-            this.btCompletar = new System.Windows.Forms.Button();
             this.dgvFamilia = new System.Windows.Forms.DataGridView();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFamilia)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btCompletar
-            // 
-            this.btCompletar.Location = new System.Drawing.Point(664, 381);
-            this.btCompletar.Name = "btCompletar";
-            this.btCompletar.Size = new System.Drawing.Size(75, 23);
-            this.btCompletar.TabIndex = 0;
-            this.btCompletar.Text = "Completar";
-            this.btCompletar.UseVisualStyleBackColor = true;
-            this.btCompletar.Click += new System.EventHandler(this.btCompletar_Click);
             // 
             // dgvFamilia
             // 
@@ -54,12 +44,12 @@ namespace A
             this.dgvFamilia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvFamilia.Location = new System.Drawing.Point(41, 42);
             this.dgvFamilia.Name = "dgvFamilia";
-            this.dgvFamilia.Size = new System.Drawing.Size(511, 409);
+            this.dgvFamilia.Size = new System.Drawing.Size(596, 409);
             this.dgvFamilia.TabIndex = 1;
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(664, 439);
+            this.btnCerrar.Location = new System.Drawing.Point(664, 428);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(75, 23);
             this.btnCerrar.TabIndex = 2;
@@ -69,7 +59,7 @@ namespace A
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(664, 115);
+            this.btnAgregar.Location = new System.Drawing.Point(664, 42);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 3;
@@ -89,7 +79,7 @@ namespace A
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(664, 226);
+            this.btnEliminar.Location = new System.Drawing.Point(664, 305);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 5;
@@ -97,33 +87,43 @@ namespace A
             this.btnEliminar.UseVisualStyleBackColor = true;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "sistema de gestion de familia ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 533);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.dgvFamilia);
-            this.Controls.Add(this.btCompletar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvFamilia)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btCompletar;
         private System.Windows.Forms.DataGridView dgvFamilia;
         private System.Windows.Forms.Button btnCerrar;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
